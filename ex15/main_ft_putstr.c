34b3a9c529de212a186e7fa6b3dec8b0b15ba9e8
Virgile LHOMME <vlhomme@e3r5p2.42.fr>
2018-11-06 15:04:01 +0100

    ft_putstr

---
+++ ex15/main_ft_putstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlhomme <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/06 14:48:25 by vlhomme           #+#    #+#             */
+/*   Updated: 2018/11/06 14:53:33 by vlhomme          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(void)
+{
+	char *test = "abcdefghijklmnopqrstuvwxyz";
+
+	ft_putstr(test);
+	return (0);
+}
