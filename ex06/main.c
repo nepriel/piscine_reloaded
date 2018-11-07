3313cb693fbd7839502357445332785b54a487f4
Virgile LHOMME <vlhomme@e3r8p19.42.fr>
2018-11-07 19:47:16 +0100

    ft_print_alphabet.c

--- ex06/main.c
+++ ex06/main.c
@@ -1,33 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alpabet.c                                 :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vlhomme <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/06 10:00:53 by vlhomme           #+#    #+#             */
-/*   Updated: 2018/11/06 10:08:56 by vlhomme          ###   ########.fr       */
+/*   Created: 2018/11/07 19:37:07 by vlhomme           #+#    #+#             */
+/*   Updated: 2018/11/07 19:39:02 by vlhomme          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "unistd.h"
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_print_alphabet(void)
-{
-	char i;
-
-	i = 'a';
-	while (i != ('z' + 1))
-	{
-		ft_putchar(i);
-		i++;
-	}
-}
+void	ft_putchar(char c);
+void	ft_print_alphabet(void);
 
 int		main(void)
 {
