6e5d2f4810f7fad041fd81382fe53043222abfd6
Virgile LHOMME <vlhomme@e3r7p1.42.fr>
2018-11-07 12:34:57 +0100

    Makefile

---
+++ ex24/test_make_file/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+gcc -L./ -lft -o out test_main.c
---
+++ ex24/test_make_file/test_main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_main.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlhomme <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/07 11:54:38 by vlhomme           #+#    #+#             */
+/*   Updated: 2018/11/07 12:10:35 by vlhomme          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+int main()
+{
+	ft_putchar('O');
+	return (0);
+}
